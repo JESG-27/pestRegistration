33795092630e7630a1a1c7798aa524ebd29d003d
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2024-03-08 14:01:27 -0600

    Vista de rutas con plantilla

--- resources/views/Pest/index.blade.php
+++ resources/views/Pest/index.blade.php
@@ -1,17 +1,17 @@
 <x-crud-layout>
-    <div>
-        <h1>Listado de registro de plagas</h1>
-        <a href="{{ route('pest.create') }}">Agregar Registro</a>
-        <table>
+    <h1>Listado de registro de plagas</h1>
+    <div class="card-body px-0 pb-2">
+        <div class="table-responsive p-0">
+            <table class="table align-items-center mb-0">
             <th>
                 <tr>
-                    <td>ID</td>
-                    <td>Cultivo</td>
-                    <td>Enfermadad/Plaga</td>
-                    <td>Ubicación</td>
-                    <td>Nivel</td>
-                    <td>Comentario</td>
-                    <td>Acciones</td>
+                    <td class="text-uppercase text-secondary text-s font-weight-bolder opacity-7">ID</td>
+                    <td class="text-uppercase text-secondary text-s font-weight-bolder opacity-7">Cultivo</td>
+                    <td class="text-uppercase text-secondary text-s font-weight-bolder opacity-7">Enfermadad/Plaga</td>
+                    <td class="text-uppercase text-secondary text-s font-weight-bolder opacity-7">Ubicación</td>
+                    <td class="text-uppercase text-secondary text-s font-weight-bolder opacity-7">Nivel</td>
+                    <td class="text-uppercase text-secondary text-s font-weight-bolder opacity-7">Comentario</td>
+                    <td class="text-uppercase text-secondary text-s font-weight-bolder opacity-7">Acciones</td>
                 </tr>
             </th>
             <tbody>
@@ -23,12 +23,13 @@
                     <td>{{ $pest -> location }}</td>
                     <td>{{ $pest -> level }}</td>
                     <td>{{ $pest -> comment }}</td>
-                    <td>
+                    <td class="badge badge-sm bg-gradient-success">
                         <a href="{{ route('pest.show', $pest) }}">Ver</a>
                     </td>
                 </tr>
                 @endforeach
             </tbody>
-        </table>
+            </table>
+        </div>
     </div>
 </x-crud-layout>
